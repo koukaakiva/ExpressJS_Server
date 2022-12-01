3aed54b7887b647d4514d896370febe6df21abf1
Koukaakiva <koukaakiva@gmail.com>
2022-12-01 11:11:46 -0500

    Four endpoints that do four things.

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  // res.status(200).send("Hello World");
+  res.render("index");
+});
+
+app.get("/numberGuesser/:n", (req, res) => {
+  // res.status(200).send("Hello World");
+  res.render("numberGuesser", { number: req.params.n });
+});
+
+app.get("/random", (req, res) => {
+  // res.status(200).send("Hello World");
+  res.render("random", { number: Math.floor(Math.random() * 100) + 1 });
+});
+
+app.get("/educationalVideo", (req, res) => {
+  // res.status(200).send("Hello World");
+  res.render("educationalVideo");
+});
 
 app.listen(3000);
