17a07aa1aed40c9f5e32565536342d5976dcb751
Koukaakiva <koukaakiva@gmail.com>
2022-12-01 11:13:47 -0500

    Cleaned code.

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/* Using JS and NodeExpress (https://expressjs.com/) build a server that has at least 4 working endpoints that all do different things
+(at least one endpoint should take in input from the client) AND the server will dish at least 2 different html pages. */
+
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
@@ -8,17 +11,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/numberGuesser/:n", (req, res) => {
-  // res.status(200).send("Hello World");
   res.render("numberGuesser", { number: req.params.n });
 });
 
 app.get("/random", (req, res) => {
-  // res.status(200).send("Hello World");
   res.render("random", { number: Math.floor(Math.random() * 100) + 1 });
 });
 
 app.get("/educationalVideo", (req, res) => {
-  // res.status(200).send("Hello World");
   res.render("educationalVideo");
 });
 
